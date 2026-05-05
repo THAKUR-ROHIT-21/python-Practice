efeaf21a59b65e24ea2297868c0e2d0756095b29
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-05-05 16:20:11 +0530

    If Else Elif Statement

---
+++ if_Else.py
@@ -0,0 +1,34 @@
+# I Solve Membership Operators using If Else
+
+str = "This is If and Else. "
+find = "This"
+
+if find in str :
+    print("It's True")
+else:
+    print("It is not True")
+print("_"*100)
+
+# I solve Arithmetic Operators using if else
+
+Age = 18
+nation = "India"
+user_age= int(input(" Enter Your Age :-"))
+user_nation=input("Enter your Nation :- ")
+
+if user_age >= Age and user_nation==nation:
+    print("Your are valid for voting")
+else:
+    print("Your are not valid for voting") 
+
+print("_"*100)
+
+age1= 18
+user_age1= int(input("Enter your age :- "))
+
+if user_age1 >= age1 :
+      print("Your can drive the car")
+else:
+    print("You can't Drive the car")
+
+print("_"*100) 
---
+++ if_elif_else.py
@@ -0,0 +1,10 @@
+str = input("Enter traffic light color :-")
+
+if str == "Red" :
+	print("Wait for green light")
+elif str == "Yellow" :
+	print("Ready for go")
+elif str == "Green" :
+	print("Go for your destination")
+else :
+	print("Your traffic Color is not match")
