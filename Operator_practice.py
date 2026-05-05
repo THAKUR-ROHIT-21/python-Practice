577054d9502fe67ae0757988a8bdd82b278be2dc
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-05-05 14:59:57 +0530

    Operator practice

---
+++ Operator_practice.py
@@ -0,0 +1,73 @@
+# Arithmetic Operators
+"""
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+#Assignment Operators
+x = 5
+
+print(x)
+
+# Comparison Operators
+
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#Logical Operators
+x = 5
+
+print(x > 0 and x < 10)
+
+# Identity Operators
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+
+print(x is z)
+print(x is y)
+print(x == y) """
+
+# Membership Operators (in, not in)
+
+# in operator
+
+str=("This is membership operator")
+find1= "This"
+find2= "is"
+find3= "operator"
+find4= "is This"
+print(find1 in str)
+print(find2 in str)
+print(find3 in str)
+print(find4 in str)
+print("_"*50)
+
+# not in operator
+
+str1= ("Use of not in operator")
+f1=("of")
+f2=("in")
+f3=("not of")
+f4=("of Use")
+print(f1 not in str1)
+print(f2 not in str1)
+print(f3 not in str1)
+print(f4 not in str1)
+
+
