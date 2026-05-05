3cfbefb8e90220777f6c7f277c70cddf1acd5a74
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-05-05 15:10:52 +0530

    Operator in Python

--- Operator_practice.py
+++ Operator_practice.py
@@ -9,11 +9,13 @@ print(x / y)
 print(x % y)
 print(x ** y)
 print(x // y)
+print("_"*100)
 
 #Assignment Operators
 x = 5
 
 print(x)
+print("_"*100)
 
 # Comparison Operators
 
@@ -26,11 +28,13 @@ print(x > y)
 print(x < y)
 print(x >= y)
 print(x <= y)
+print("_"*100)
 
 #Logical Operators
 x = 5
 
 print(x > 0 and x < 10)
+print("_"*100)
 
 # Identity Operators
 
@@ -41,6 +45,8 @@ z = x
 print(x is z)
 print(x is y)
 print(x == y)
+print("_"*100)
+
 
 # Membership Operators (in, not in)
 
@@ -55,7 +61,7 @@ print(find1 in str)
 print(find2 in str)
 print(find3 in str)
 print(find4 in str)
-print("_"*50)
+print("_"*100)
 
 # not in operator
 
@@ -68,5 +74,6 @@ print(f1 not in str1)
 print(f2 not in str1)
 print(f3 not in str1)
 print(f4 not in str1)
+print("_"*100)
 
 
