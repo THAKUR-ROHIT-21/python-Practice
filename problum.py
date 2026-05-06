603d6cc8513b5eaae3274d7a6c4b77dd5df9b8b9
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-05-06 10:40:51 +0530

    Practice

---
+++ problum.py
@@ -0,0 +1,15 @@
+
+
+# >= is not supported in (int and tuple)
+num4= 3
+num5= 9
+num6= 12
+number= (num4,num5,num6)
+if num4 >= number: 
+    print("num4 is grater")
+elif num5 >= number: 
+    print("num5 is grater")
+elif num6 >= number: 
+    print("num6 is grater") 
+else: 
+    print("False")
