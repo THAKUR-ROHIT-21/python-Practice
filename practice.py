b44723a41c5e6d56304edcd3f91e335fdaa24374
THAKUR-ROHIT-21 <r7256870923@gmail.com>
2026-05-11 23:21:57 +0530

    Logic

--- practice.py
+++ practice.py
@@ -1,14 +1,21 @@
 # it's Dictionary Comprehension.
 
-def num(base,exp):
-    if exp ==0:
-        return 1
-    return base * num(base,exp-1)
-print(num(2,3))
+# def num(base,exp):
+#     if exp ==0:
+#         return 1
+#     return base * num(base,exp-1)
+# print(num(2,3))
 
 # Using pow() Function method
 # in power function base **exp it means : 4**6 :- 4x4x4x4x4x4=4096
-base= 4
-exp = 6
-result= pow(base,exp)
-print(f"power {result}")
+# base= 4
+# exp = 6
+# result= pow(base,exp)
+# print(f"power {result}")
+
+# Logic method in python
+
+for i in range(1,10):
+    for j in range(0,i):
+        print("*",end=" ")
+    print(" ")
